5c5496bac4f32dfb6a78df42fb5c6bcc2d16c222
parrwgrah <parrgraham888@gmail.com>
2025-05-24 16:33:27 +1200

    initial commit

---
+++ abstractdb.hpp
@@ -0,0 +1,28 @@
+/**
+ * abstractdb.hpp
+ * C++ header file that should contain declaration for
+ * - struct movie (given)
+ * - AbstractDbTable abstract class
+ * 
+ * You need to modify this file to declare AbstractDbTable abstract class 
+ * as specified in the hand-out (Task 1)
+ */ 
+
+
+
+#ifndef __ABSTRACT_DB_HPP__
+#define __ABSTRACT_DB_HPP__
+
+#include <string>
+
+namespace nwen 
+{
+    struct movie {
+        unsigned long id;
+        char title[50];
+        unsigned short year;
+        char director[50];
+    };
+}
+
+#endif /* __ABSTRACT_DB_HPP__ */
