c7504b9b03ac1e62e62ab7f38fa36b6b2aac570f
parrwgrah <parrgraham888@gmail.com>
2025-05-25 17:39:40 +1200

    This is an update to save and load ,this isnt going to get done tonight but i might be able to do tomorrow moring

--- abstractdb.cpp
+++ abstractdb.cpp
@@ -9,14 +9,100 @@
  */ 
 
 #include "abstractdb.hpp"
+
+#include <cstring>
 #include<string>
+#include<fstream>
+#include <sstream>
+
+
+
 namespace nwen {
-    bool AbstractDbTable::loadCSV(const std::string In){
-        return false;
+
+    //-----------------------------------------------------------------------------------------
+    // Load
+    bool AbstractDbTable::loadCSV(const std::string& filename) {
+        std::ifstream inFile(filename);
+        if (!inFile.is_open()) {
+            return false; // File couldn't be opened
+        }
+
+        std::string line;
+        while (std::getline(inFile, line)) {
+            std::stringstream ss(line);
+            std::string idStr, title, yearStr, director;
+
+            // Read ID
+            if (!std::getline(ss, idStr, ',')) return false;
+
+            // Read Title
+            if (!std::getline(ss, title, ',')) return false;
+            if (title.front() == '"' && title.back() == '"') {
+                title = title.substr(1, title.size() - 2); // Remove quotes
+            }
+
+            // get Year
+            if (!std::getline(ss, yearStr, ',')) return false;
+
+            // get Director
+            if (!std::getline(ss, director)) return false;
+            if (director.front() == '"' && director.back() == '"') {
+                director = director.substr(1, director.size() - 2); // Remove quotes
+            }
+
+            // Ensure Id and Year are ints
+            int id, year;
+            try {
+                id = std::stoi(idStr);
+                year = std::stoi(yearStr);
+            } catch (const std::exception&) {
+                return false;
+            }
+
+            // Build movie object ,requires us to switch the data types again
+            // movie m = { id, title, year, director };
+            movie m;
+            std::copy(title.begin(), title.end(), m.title);
+            m.title[std::min(title.size(), sizeof(m.title) - 1)] = '\0';
+
+            std::copy(director.begin(), director.end(), m.director);
+            m.director[std::min(director.size(), sizeof(m.director) - 1)] = '\0';
+            if (!this->add(m)) {
+                continue;
+            }
+        }
+
+        inFile.close();
+        return true;
     }
-    bool AbstractDbTable::saveCSV(const std::string In){
-        return false;
+
+
+    //--------------------------------------------------------------------------------
+    //Save
+
+    bool AbstractDbTable::saveCSV(const std::string& filename) {
+        std::ofstream outFile(filename, std::ios::out);
+        if (!outFile.is_open()) {
+            return false;
+        }
+            for (int i = 0; i < this->rows(); ++i) {
+        movie* m = this->get(i);
+        if (m == nullptr) continue;
+
+        outFile << m->id << ","
+                << "\"" << m->title << "\"" << ","
+                << m->year << ","
+                << "\"" << m->director << "\"" << "\n";
+
+        if (outFile.fail()) {
+            outFile.close();
+            return false;
+        }
     }
+
+    outFile.close();
+    return true;
+}
 
 
 
